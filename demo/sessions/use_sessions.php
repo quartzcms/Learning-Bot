af78288be684a5cadff91f599155d46f538258d7
Alexandre Lefebvre <quartzcms@gmail.com>
2019-08-05 16:23:36 -0400

    changes to the cron file

--- demo/sessions/use_sessions.php
+++ demo/sessions/use_sessions.php
@@ -1,7 +1,8 @@
 <?php 
 	function use_session($session_name) {
+		$_SERVER['REMOTE_ADDR'] = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '255.255.255.255';
 		$file_path = __DIR__.'/data/'.$_SERVER['REMOTE_ADDR'].'-'.date('Ymd').'.txt';		
-		$content = file_get_contents($file_path);
+		$content = @file_get_contents($file_path);
 		$array_of_session = explode("\n", $content);
 		
 		foreach($array_of_session as $key => $line){
